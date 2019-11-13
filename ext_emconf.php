3dbfa8abd178358bb7388d851274a23d435506bf
NITSAN <sanjay@nitsan.in>
2019-11-13 13:20:49 +0530

    [TODO] Documentation Update!

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => '[NITSAN] All In One Lightbox/Modalbox',
-	'description' => 'One of the only TYPO3 extension which provides to use most popular jQuery Lightbox/Modalbox at TYPO3 content elements. This TYPO3 extension provides to configure many jQuery plugins eg., lightbox2, fancybox2, colorbox, prettyphoto, darkbox, magnific-popup & more will be available in an upcoming version.',
+	'description' => 'One of the only TYPO3 extension which provides to use most popular jQuery Lightbox/Modalbox at TYPO3 content elements. This TYPO3 extension provides to configure many jQuery plugins eg., lightbox2, fancybox2, colorbox, prettyphoto, darkbox, magnific-popup. You can download PRO version for more-features & free-support at https://t3terminal.com/ns-all-in-one-lightbox-pro/',
 	'category' => 'plugin',
 	'author' => 'T3:Bhavin Barad, T3:Keval Pandya, QA:Siddharth Sheth',
 	'author_email' => 'sanjay@nitsan.in',
@@ -12,11 +12,11 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '4.0.0',
+	'version' => '4.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '7.0.0-9.5.99',
-			'fluid_styled_content' => '',
+			'fluid_styled_content' => '7.0.0-9.5.99',
 		),
 		'conflicts' => array(
 		),
