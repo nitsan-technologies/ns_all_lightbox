f5c610087a2c6302a3e5791ec79cdd3b14c642e8
Rohan Parmar <rohan.nitsan@gmail.com>
2023-06-23 12:07:51 +0530

    [TASK] v12 compitable

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,25 +1,24 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => '[NITSAN] All In One Lightbox/Modalbox',
-	'description' => 'One of the only TYPO3 extension which provides to use most popular jQuery Lightbox/Modalbox at TYPO3 content elements. This TYPO3 extension provides to configure many jQuery plugins eg., lightbox2, fancybox2, colorbox, prettyphoto, darkbox, magnific-popup. Live-Demo: https://demo.t3planet.com/t3t-extensions/lightbox/default-lightbox/ You can download PRO version for more-features & free-support at https://t3planet.com/typo3-lightbox-extension',
-	'category' => 'plugin',
-	'author' => 'NITSAN Technologies Pvt Ltd',
-	'author_email' => 'sanjay@nitsan.in',
-	'author_company' => 'NITSAN Technologies Pvt Ltd',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '5.0.5',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.0.0-11.5.99',			
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => '[NITSAN] All In One Lightbox/Modalbox',
+    'description' => 'One of the only TYPO3 extension which provides to use most popular jQuery Lightbox/Modalbox at TYPO3 content elements. This TYPO3 extension provides to configure many jQuery plugins eg., lightbox2, fancybox2, colorbox, prettyphoto, darkbox, magnific-popup. Live-Demo: https://demo.t3planet.com/t3t-extensions/lightbox/default-lightbox/ You can download PRO version for more-features & free-support at https://t3planet.com/typo3-lightbox-extension',
+    'category' => 'plugin',
+    'author' => 'Team NITSAN',
+    'author_email' => 'sanjay@nitsan.in',
+    'author_company' => 'NITSAN Technologies Pvt Ltd',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'version' => '12.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '7.0.0-12.9.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
