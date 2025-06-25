27b8b6d2226d7a4b66025da80691cab937da2cdb
NITSAN <sanjay@nitsan.in>
2025-06-25 12:08:15 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,13 @@
 <?php
 
 $EM_CONF['ns_all_lightbox'] = [
-    'title' => 'All In One Lightbox',
-    'description' => 'Introducing the TYPO3 All-in-One Extension, a plugin that effortlessly integrates popular jQuery slider plugins into your website with just a few clicks! Enjoy a range of slider options including Nivo Slider, Royal Slider, Owlcarousel, Sliderjs Slider, and more.
+    'title' => 'TYPO3 All-in-One Lightbox & Sliders',
+    'description' => 'Seamlessly integrate multiple jQuery sliders like Nivo, Royal Slider, OwlCarousel, and more into your TYPO3 site – all in one lightweight extension.',
 
-	*** Live Demo: https://demo.t3planet.com/t3-extensions/all-lightbox *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-lightbox-extension',
     'category' => 'plugin',
-    'author' => 'T3: Rohan Parmar, Divya Goklani, Nilesh Malankiya, QA: Krishna Dhapa',
-    'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'T3Planet // NITSAN',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet	',
     'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '0',
